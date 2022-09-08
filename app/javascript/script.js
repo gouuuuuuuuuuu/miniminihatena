575cf0e429c2f21bfded9795ea0252c0076f8f1e
EC2 Default User <ec2-user@ip-172-31-23-43.ec2.internal>
2022-09-08 21:07:59 +0000

    35

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -31,3 +31,13 @@ $(function(){
     });
   });
 });
+
+
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
