b0c81a8784333471e905b48b62b51c78d86e0870
EC2 Default User <ec2-user@ip-172-31-23-43.ec2.internal>
2022-09-12 21:13:05 +0000

    40

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,17 +1,20 @@
 class Public::HomesController < ApplicationController
   def top
-    if params[:genre_id].nil?
-      @posts = Post.all.order(created_at:"desc")
-    else
+    @posts = Post.all.order(created_at:"desc")
+    if params[:genre_id].present?
        #binding.pry
-    post_ids = PostGenre.where(genre_id: params[:genre_id]).pluck(:post_id)
-@posts = Post.where(id: post_ids)
+     post_ids = PostGenre.where(genre_id: params[:genre_id]).pluck(:post_id)
+     @posts = @posts.where(id: post_ids)
+    end
+    if params[:word].present?
+      @posts = @posts.where("title LIKE?","%#{params[:word]}%")
     end
     require 'news-api'
     news = News.new(ENV['NEWS_API_KEY'])
     @news = news.get_top_headlines(country: 'jp')
     @genres = Genre.all
     gon.weather_api_key = ENV['WEATHER_API_KEY']
+    @count = @posts.count
   end
 
   def about
