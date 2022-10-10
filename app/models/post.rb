9c43bb212bbc0be45a3602e038d7183401758148
EC2 Default User <ec2-user@ip-172-31-46-213.ap-northeast-1.compute.internal>
2022-10-10 12:48:00 +0000

    jpegの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,7 +13,7 @@ class Post < ApplicationRecord
 
   def get_post_image
     unless post_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       post_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     post_image
