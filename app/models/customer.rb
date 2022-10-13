a42ef80f62e7a980523f3ed148656fcc180b8575
EC2 Default User <ec2-user@ip-172-31-46-213.ap-northeast-1.compute.internal>
2022-10-13 10:19:22 +0000

    1

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,14 +12,13 @@ class Customer < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments
-   has_many :genres, dependent: :destroy
+  has_many :genres, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
   
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ , message: 'はカタカナで入力して下さい。'}
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ , message: 'はカタカナで入力して下さい。'}
-  
-   validates :nickname, {presence: true, length: {maximum: 15}}
+  validates :nickname, {presence: true, length: {maximum: 15}}
   
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :customer
-   validates_uniqueness_of :post_id, scope: :customer_id
+  validates_uniqueness_of :post_id, scope: :customer_id
 end
