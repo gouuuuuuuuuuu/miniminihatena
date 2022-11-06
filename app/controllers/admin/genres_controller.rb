a8ae521779aaeaa39afd9b550e331f7d2ca15e12
EC2 Default User <ec2-user@ip-172-31-46-213.ap-northeast-1.compute.internal>
2022-11-06 11:58:36 +0000

    あ

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,46 +1,46 @@
 class Admin::GenresController < ApplicationController
   before_action :authenticate_customer
   
-def index
+ def index
   @genres = Genre.where(customer_id: nil)
-end
-def by_customer_index
- @genres = Genre.where.not(customer_id: nil)
-end
-
-def edit
- @genre=Genre.find(params[:id])
-end
-
-def update
- @genre = Genre.find(params[:id])
- @genre.update(genre_params)
- if @genre.customer_id.nil?
-  redirect_to admin_genres_path(@genre[:id])
- else
-  redirect_to admin_genres_by_customer_index_path
  end
-end
-
-def create
- @genres = Genre.new(genre_params)
- @genres.save
- redirect_to admin_genres_path(@genres)
-end
-
-def destroy
- @genre= Genre.find(params[:id])
- @genre.destroy
- if params[:mode] == 'genres'
-  redirect_to admin_genres_path
- else
-  redirect_to admin_genres_by_customer_index_path
+ def by_customer_index
+  @genres = Genre.where.not(customer_id: nil)
  end
-end
 
-private
+ def edit
+  @genre=Genre.find(params[:id])
+ end
 
-def genre_params
- params.require(:genre).permit(:name)
-end
+ def update
+  @genre = Genre.find(params[:id])
+  @genre.update(genre_params)
+  if @genre.customer_id.nil?
+   redirect_to admin_genres_path(@genre[:id])
+  else
+   redirect_to admin_genres_by_customer_index_path
+  end
+ end
+
+ def create
+  @genres = Genre.new(genre_params)
+  @genres.save
+  redirect_to admin_genres_path(@genres)
+ end
+
+ def destroy
+  @genre= Genre.find(params[:id])
+  @genre.destroy
+  if params[:mode] == 'genres'
+   redirect_to admin_genres_path
+  else
+   redirect_to admin_genres_by_customer_index_path
+  end
+ end
+
+ private
+
+ def genre_params
+  params.require(:genre).permit(:name)
+ end
 end
